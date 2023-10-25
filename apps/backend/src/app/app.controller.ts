88663c5a1df9f2673fe7515dc125f686158ce183
wimkung <wimkung@gmail.com>
2023-10-25 16:58:08 +0700

    init project

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import {HealthCheckService, MongooseHealthIndicator} from '@nestjs/terminus';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private health: HealthCheckService, private mongooseHealthIndicator: MongooseHealthIndicator) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('health')
+  getHealth() {
+    return this.health.check([
+      () => this.mongooseHealthIndicator.pingCheck('mongo')
+    ])
   }
 }
--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-
+import { env } from 'node:process'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from '@nestjs/mongoose';
+import {TerminusModule} from '@nestjs/terminus';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => {
+        return {
+          uri: env.MONGO_URI ?? 'mongodb://mongo/example',
+          authSource: 'admin'
+        }
+      }
+    }),
+    TerminusModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
